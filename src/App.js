58c3c8303dc00d36c8d272ebf22b3b1e013ad322
suubcity <suubcity@outlook.com>
2020-11-23 14:06:03 -0700

    navbar created and linked through react router

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        App
+        
         <Nav />
         <Switch>
           <Route exact path="/" component={Home} />
--- src/modules/Nav.js
+++ src/modules/Nav.js
@@ -1,6 +1,22 @@
 import React from "react";
+import "./nav.css";
+import { Link } from "react-router-dom";
+
 function Nav() {
-  return <div>Nav</div>;
+  return (
+    <div>
+      <nav id="navContainer">
+        <ul id="navigation">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/Shop">
+            <li>Shop</li>
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default Nav;
